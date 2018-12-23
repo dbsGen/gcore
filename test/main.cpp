fc08e51f93e92bef9aa362a27bec44c07c1d6bdc
Gen2 <l2e@protonmail.com>
2018-12-23 11:20:40 +0700

    Added gtest and some test unit

--- test/main.cpp
+++ test/main.cpp
@@ -6,17 +6,94 @@
 #include <core/Callback.h>
 #include <core/math/Type.h>
 
+#include <gtest/gtest.h>
+
 using namespace gcore;
 
-int main() {
+#define STR(S) #S
+
+TEST(Object, NameOfTestObject)
+{
+    Ref<TestObject> obj(new TestObject());
+    EXPECT_STREQ(obj->getClass()->getFullname().str(), STR(TestObject));
+}
+
+TEST(Object, SetterAndGetter)
+{
+    Ref<TestObject> obj(new TestObject());
+    obj->callArgs("setIntValue", 1023);
+    EXPECT_EQ((int)obj->call("getIntValue"), obj->getIntValue());
+}
+
+TEST(Object, Property)
+{
+    Ref<TestObject> obj(new TestObject());
+    const Property *property = obj->getInstanceClass()->getProperty("int_value");
+    property->set(obj, 1023);
+    EXPECT_EQ((int)property->get(obj), 1023);
+}
+
+TEST(Variant, Transform)
+{
+    Variant f_variant(30.32);
+    EXPECT_EQ((int)f_variant, 30);
+
+    Variant v2_variant(Vector2f(2,3));
+    Vector2f vec2 = v2_variant;
+    Vector3f vec3 = v2_variant;
+    EXPECT_EQ(vec2[0], 2);
+    EXPECT_EQ(vec2[1], 3);
+
+    EXPECT_EQ(vec3[0], 0);
+    EXPECT_EQ(vec3[1], 0);
+    EXPECT_EQ(vec3[2], 0);
+}
+
+TEST(Callback, Params)
+{
+    int target;
+    RCallback cb = C([&](int l){
+        EXPECT_EQ(l, target);
+    });
+
+    target = 30;        cb(30.32);
+    target = 2883;      cb(2883, "1");
+
+    Ref<TestObject> obj(new TestObject());
+    const Class *clz;
+    RCallback cb2 = C([&](Object *object){
+        if (object) {
+            EXPECT_EQ(object->getInstanceClass(), clz);
+        }else {
+            EXPECT_EQ(clz, nullptr);
+        }
+    });
+    clz = NULL;                     cb2(30.32);
+    clz = NULL;                     cb2(2883, obj);
+    clz = _String::getClass();      cb2("nihao");
+    clz = TestObject::getClass();   cb2(obj);
+
+}
+
+TEST(Callback, ParamsTransform)
+{
+    Vector2f test_v2;
+
+    Vector2f v2(2,3);
+    Vector3f v3(2,3,4);
+    RCallback cb3 = C([&](Vector2f v){
+        EXPECT_EQ(test_v2, v);
+    });
+    test_v2 = v2;           cb3(v2);
+    test_v2 = Vector2f();   cb3(v3);
+}
+
+int main(int argc, char* argv[]) {
     Ref<TestObject> obj(new TestObject());
 
     printf("ClassName %s\n", obj->getInstanceClass()->getFullname().str());
 
     printf("ClassName %s\n", obj->getInstanceClass()->getParent()->getFullname().str());
-
-    obj->callArgs("setIntValue", 1023);
-    printf("int value is %d -> %d\n", (int)obj->call("getIntValue"), obj->getIntValue());
 
 #define PrintSize(CLASS) printf(#CLASS " size %d\n", sizeof(CLASS))
     PrintSize(StringName);
@@ -52,5 +129,8 @@ int main() {
     cb3(v2);
     cb3(v3);
 
-    return 0;
+
+    testing::InitGoogleTest(&argc, argv);
+
+    return RUN_ALL_TESTS();
 }
