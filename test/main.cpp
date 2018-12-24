4aea81b351e0a1fa95438ef09a5b141f554f4f0e
Gen2 <l2e@protonmail.com>
2018-12-24 10:16:28 +0700

    move script to gcore

--- test/main.cpp
+++ test/main.cpp
@@ -5,17 +5,21 @@
 #include <vector>
 #include <core/Callback.h>
 #include <core/math/Type.h>
+#include <script/ruby/RubyScript.h>
 
 #include <gtest/gtest.h>
 
 using namespace gcore;
+using namespace gscript;
+
+#define PATH "/Users/gen2/Programs/grender/gcore/test/ruby"
 
 #define STR(S) #S
 
 TEST(Class, ClassRelationships)
 {
     EXPECT_EQ(TestObject::getClass()->getParent(), RefObject::getClass());
-    EXPECT_TRUE(TestObject::getClass()->isTypeOf(RefObject::getClass()));
+    EXPECT_TRUE(TestObject::getClass()->isTypeOf(Object::getClass()));
     EXPECT_TRUE(TestObject::getClass()->isSubclassOf(RefObject::getClass()));
     EXPECT_FALSE(TestObject::getClass()->isSubclassOf(TestObject::getClass()));
 }
@@ -102,6 +106,37 @@ TEST(Size, ObjectMinnumSize)
     EXPECT_EQ(sizeof(Reference), 8);
     EXPECT_EQ(sizeof(Ref<TestObject>), 8);
     EXPECT_EQ(sizeof(Variant), 24);
+}
+
+
+TEST(Ruby, RunSimpleScript)
+{
+    RubyScript ruby;
+    EXPECT_EQ((int)ruby.runScript("1+2"), 3);
+    RArray arr = ruby.runScript("[1,'hello', 3]");
+
+    EXPECT_EQ((int)arr[0], 1);
+    EXPECT_STREQ((const char *)arr[1], "hello");
+    EXPECT_EQ((int)arr[2], 3);
+}
+
+TEST(Ruby, RunEnvFile)
+{
+    Ref<TestObject> obj;
+
+    RubyScript ruby;
+    ruby.setup(PATH);
+
+    ruby.run(PATH "/test.rb");
+
+    obj = ruby.runScript("$to");
+
+    EXPECT_EQ(obj->getIntValue(), 333);
+
+    obj->setCallback(C([](const std::string &str){
+        EXPECT_STREQ(str.c_str(), "InRuby");
+    }));
+    ruby.runScript("$to.call_cb");
 }
 
 int main(int argc, char* argv[]) {
